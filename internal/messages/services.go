096eaf0f9ac851dd9e880809a108ecea160b9249
kalalannn <vorobievnikolaj07@gmail.com>
2025-02-22 14:17:28 +0100

    messages update

--- internal/messages/services.go
+++ internal/messages/services.go
@@ -1,8 +1,8 @@
 package messages
 
 const (
-	NewWebSocketClient                 = "Added new WebSocket client"
-	RemoveWebSocketClient              = "Removed WebSocket client"
+	NewWebSocketClient                 = "Added new WebSocket client."
+	RemoveWebSocketClient              = "Removed WebSocket client."
 	BroadcastFailed                    = "Failed to send update to client: %s, disconnecting\n"
 	StopBroadcastUpdatesStart          = "Stopping broadcast updates..."
 	StopBroadcastUpdatesEnd            = "Broadcast updates stopped."
